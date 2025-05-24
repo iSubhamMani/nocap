cf147cf174b879371ea41d804b2fc8f18cc49fd1
Subham Mani <manisubham09@gmail.com>
2025-05-24 22:39:01 +0530

    config

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,4 +8,6 @@ export default authkitMiddleware({
 });
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ["/", "/home", "/login"] };
+export const config = {
+  matcher: ["/", "/events", "/discover", "/host-event", "/login"],
+};
