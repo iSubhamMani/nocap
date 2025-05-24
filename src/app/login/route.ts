8b78673e8ac53e95322f60b603370661decce236
Subham Mani <manisubham09@gmail.com>
2025-05-24 20:57:54 +0530

    add auth check in login page

--- src/app/login/route.ts
+++ src/app/login/route.ts
@@ -1,7 +1,13 @@
-import { getSignInUrl } from "@workos-inc/authkit-nextjs";
+import { getSignInUrl, withAuth } from "@workos-inc/authkit-nextjs";
 import { redirect } from "next/navigation";
 
 export const GET = async () => {
+  const { user } = await withAuth();
+
+  if (user) {
+    return redirect("/home");
+  }
+
   const signInUrl = await getSignInUrl();
 
   return redirect(signInUrl);
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,9 @@ import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 export default authkitMiddleware({
   middlewareAuth: {
     enabled: true,
-    unauthenticatedPaths: ["/"],
+    unauthenticatedPaths: ["/", "/login"],
   },
 });
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ["/", "/home"] };
+export const config = { matcher: ["/", "/home", "/login"] };
